5a85c5e6204d1bac3f6baf4235c196c80e6eee3b
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-09-03 16:16:31 +0200

    end exercise and bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            title: 'VUE INTRO'
+            title: 'VUE INTRO',
+            imageUrl: `https://miro.medium.com/v2/resize:fit:1100/format:webp/1*XH9l-6x9SUlmGxPlZFaoIA.jpeg`
         }
     }
 }).mount('#app')
